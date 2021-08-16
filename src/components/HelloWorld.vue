14fa9fe2e5a50790f5bcda644bd79b1c4318098a
Cosmin Jescu <57216949+jesq@users.noreply.github.com>
2021-08-16 23:04:46 +0300

    Added middle section items

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,19 +18,41 @@
         </ul>
       </nav>
     </header>
+
+    <section class="content-section">
+      <div class="content-wrapper">
+        <img class="logo-img" src="@/assets/logo.jpg" alt="Google Logo" />
+        <div class="search-bar">
+          <fa icon="search"></fa>
+          <input class="search-input" type="text">
+          <fa icon="keyboard"></fa>
+          <fa icon="microphone"></fa>
+        </div>
+      </div>
+      <div class="search-btns">
+        <button class="google-search-btn">Căutare Google</button>
+        <button class="lucky-search-btn">Mă simt norocos</button>
+      </div>
+      <div class="languages">
+        <p>
+          Google oferit în: 
+          <a href="">English</a>
+          <a href="">Magyar</a>
+          <a href="">Deutsch</a>
+        </p>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style scoped></style>
